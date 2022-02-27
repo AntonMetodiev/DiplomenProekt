b551922f0ffb76ba511050acdb191b4a79724f41
AntonMetodiev <toni2riemail@abv.bg>
2022-02-27 22:35:37 +0200

    admin changes

--- PetHotel/PetHotel/Data/ApplicationDbContext.cs
+++ PetHotel/PetHotel/Data/ApplicationDbContext.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using PetHotel.App.Domain;
+using PetHotel.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using PetHotel.App.Models;
 
 namespace PetHotel.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<PetHotelUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            this.Database.EnsureCreated();
         }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<PetHotel.App.Models.OrderListingViewModel> OrderListingViewModel { get; set; }
     }
 }
