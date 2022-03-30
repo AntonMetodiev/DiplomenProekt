d4b07874f6e344a751506efb42f4d7d02d52a6db
AntonMetodiev <toni2riemail@abv.bg>
2022-03-30 09:44:50 +0300

    and another one

---
+++ PetHotel/PetHotel/Entities/Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetHotel.App.Entities
+{
+    public class Status
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        
+        public StatusName StatusName { get; set; }
+
+        public virtual ICollection<Request> Requests { get; set; }
+    }
+}
